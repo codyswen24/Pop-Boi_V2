acfc17e84296379edf51072cfc182efe3e951ca0
Cody Swensen <codyswen24@gmail.com>
2025-11-17 12:57:29 -0700

    Add deck class with card management features
    Implemented deck class to manage a card deck with shuffling and drawing functionality.

--- FinalProject_Pop-Boi_V2/src/PopBoi/deck.java
+++ FinalProject_Pop-Boi_V2/src/PopBoi/deck.java
@@ -1,5 +1,34 @@
 package PopBoi;
 
-public class deck {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class deck {
+	
+	private ArrayList<card>  cards;
+	
+	public deck() {
+		cards = new ArrayList<>();
+		buildDeck();
+	}
+	
+	private void buildDeck () {
+		String[] suits = {"hearts", "diamonds", "spades", "clubs"};
+		String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"};
+		int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+		
+		for (int s = 0; s < suits.length; s++) {
+			for (int r = 0; r < ranks.length; r++) {
+				cards.add(new card(suits[s], ranks[r], values[r]));
+			}
+		}
+	}
+	
+	public void Shuffle() {
+		Collections.shuffle(cards);
+	}
+	
+	public card drawCard() {
+		return cards.remove(0);
+	}
 }
