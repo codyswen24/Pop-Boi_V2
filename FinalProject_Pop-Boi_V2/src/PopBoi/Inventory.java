3d63512bbb1d32e2883c64020e5c792d77a9854e
stvly <stvly@Alex>
2025-11-18 15:14:59 -0700

    Hi there 2

--- FinalProject_Pop-Boi_V2/src/PopBoi/Inventory.java
+++ FinalProject_Pop-Boi_V2/src/PopBoi/Inventory.java
@@ -2,7 +2,7 @@ package PopBoi;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-//Hi there!
+//Hi there! 2
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
