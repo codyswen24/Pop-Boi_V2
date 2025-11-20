283e1685a9fb5127997e9076b73f9c5eb58ef658
Cody Swensen <codyswen24@gmail.com>
2025-11-20 13:16:08 -0700

    Hand is FINISHED

--- FinalProject_Pop-Boi_V2/src/PopBoi/hand.java
+++ FinalProject_Pop-Boi_V2/src/PopBoi/hand.java
@@ -2,35 +2,53 @@ package PopBoi;
 
 import java.util.ArrayList;
 
+/**
+ * This class creates and maintains what cards the player and house have.
+ * @author Cody Swensen
+ */
 public class hand {
-	
+
 	private ArrayList<card> cards = new ArrayList<>();
-	
+
+	/**
+	 * adds a card to the hand
+	 * @param card
+	 * @author Cody Swensen
+	 */
 	public void add(card card) {
-        cards.add(card);
-    }
-	
+		cards.add(card);
+	}
+
+	/**
+	 * retrieves the card object 
+	 * @return cards
+	 * @author Cody Swensen
+	 */
 	public ArrayList<card> getCards() {
-        return cards;
-    }
-	
+		return cards;
+	}
+
+	/**
+	 * gets total hand values and determines the value of an ACE depending on total hand size
+	 * @return
+	 */
 	public int getValue() {
 		int value = 0;
 		int aces = 0;
-		
+
 		for (card c : cards) {
 			value += c.getValue();
 			if (c.getRank().equals("ace")) {
 				aces++;
 			}
 		}
-		
-		//checks 
+
+		// checks ace value based off total value
 		while (value > 21 && aces > 0) {
 			value -= 10;
 			aces--;
 		}
-		
+
 		return value;
 	}
 }
