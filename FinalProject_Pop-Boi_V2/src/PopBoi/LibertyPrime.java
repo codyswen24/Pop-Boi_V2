6a6cb21df85d7c007bb6a484eab4d45bfa096c83
GuntersauruasRex <spencer@worthplanning.com>
2025-11-21 23:50:22 -0700

    Update LibertyPrime.java
    updated LP

--- FinalProject_Pop-Boi_V2/src/PopBoi/LibertyPrime.java
+++ FinalProject_Pop-Boi_V2/src/PopBoi/LibertyPrime.java
@@ -2,14 +2,23 @@ package PopBoi;
 
 import java.awt.Color;
 
+/**
+ * LibertyPrime chatbot
+ * 
+ * @author SpencerS
+ */
 public class LibertyPrime extends ChatBot {
 
-	public LibertyPrime(popBoiApp app) {
+	private static final long serialVersionUID = 1L;
+	private stats playerStats;
+
+	public LibertyPrime(popBoiApp app, stats statsPanel) {
 		super(app);
+		this.playerStats = statsPanel;
 
 		// Customize chat area colors
-		chatArea.setBackground(new Color(0, 0, 50)); // dark blue background
-		chatArea.setForeground(new Color(255, 255, 0)); // bright yellow text
+		chatArea.setBackground(new Color(0, 0, 50));
+		chatArea.setForeground(new Color(255, 255, 0));
 
 		inputField.setBackground(new Color(0, 0, 70));
 		inputField.setForeground(new Color(255, 255, 0));
@@ -18,7 +27,7 @@ public class LibertyPrime extends ChatBot {
 
 	@Override
 	protected String[] getResponses() {
-		return new String[] { "DEATH TO COMMUNISTS!", "LIBERTY PRIMES NEVER SURRENDER!", "REMEMBER FREEDOM!",
+		return new String[] { "DEATH TO COMMUNISTS!", "LIBERTY PRIME NEVER SURRENDERS!", "REMEMBER FREEDOM!",
 				"ENFORCE AMERICAN VALUES!", "I AM LIBERTY PRIME!" };
 	}
 
@@ -27,13 +36,38 @@ public class LibertyPrime extends ChatBot {
 		return "Liberty Prime";
 	}
 
-	// Optional: make responses more dramatic
+	@Override
+	protected String getTitle() {
+		return "Liberty Prime";
+	}
+
+	@Override
+	protected String getIconPath() {
+		return "/PopBoi/Images/LIBERTYPRIME.png";
+	}
+
+	/**
+	 * has a chance to either heal or damage the player.
+	 */
 	@Override
 	protected String generateResponse(String userInput) {
-		// 25% chance to shout multiple times
-		if (Math.random() < 0.25) {
+		double roll = Math.random();
+
+		if (roll < 0.10 && playerStats != null) {
+			playerStats.heal(50);
+			return "LIBERTY PRIME ADMINISTERS FIRST-AID! +50 HP RESTORED!";
+		}
+
+		if (roll < 0.25 && playerStats != null) {
+			playerStats.takeDamage(25);
+			return "COMMUNIST DETECTED. EXECUTING. -25 HP!";
+		}
+
+		if (roll < 0.65) {
 			return getResponses()[0] + " " + getResponses()[0];
 		}
+
 		return super.generateResponse(userInput);
 	}
+
 }
