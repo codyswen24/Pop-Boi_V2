d86ba3e1d375f48a0175fdfef858d86eec909594
GuntersauruasRex <spencer@worthplanning.com>
2025-11-21 23:54:18 -0700

    Update Deathclaw.java
    updated DC chat bot

--- FinalProject_Pop-Boi_V2/src/PopBoi/Deathclaw.java
+++ FinalProject_Pop-Boi_V2/src/PopBoi/Deathclaw.java
@@ -2,12 +2,21 @@ package PopBoi;
 
 import java.awt.Color;
 
+/**
+ * Deathclaw chat bot
+ * 
+ * @author SpencerS
+ */
 public class Deathclaw extends ChatBot {
 
-	public Deathclaw(popBoiApp app) {
-		super(app);
+	private static final long serialVersionUID = 1L;
+	private stats playerStats;
 
-		// Optional: customize chat area colors
+	public Deathclaw(popBoiApp app, stats statsPanel) {
+		super(app);
+		this.playerStats = statsPanel;
+
+		// Customize chat area colors
 		chatArea.setBackground(new Color(30, 0, 0));
 		chatArea.setForeground(Color.RED);
 
@@ -18,23 +27,38 @@ public class Deathclaw extends ChatBot {
 
 	@Override
 	protected String[] getResponses() {
-		return new String[] { "ROAR!", "Deathclaw is approaching...", "You cannot escape me!",
-				"I am the apex predator!", "Feel my claws!" };
+		return new String[] { "ROAR!", "RAWR!!!", "GROWL!!!", "ROAR! RAWR!!!" };
 	}
 
-	@Override
-	protected String getTitle() {
-		return "Deathclaw";
-	}
-
-	// Optional: override generateResponse for custom logic
+	/**
+	 * has a chance to attack the player
+	 */
 	@Override
 	protected String generateResponse(String userInput) {
-		// Example: Deathclaw sometimes roars twice
-		if (Math.random() < 0.3) {
-			return getResponses()[0] + " " + getResponses()[0]; // double roar
+		double chance = Math.random();
+
+		// 10% chance to attack player
+		if (chance < 0.10 && playerStats != null) {
+			int damage = 20;
+			playerStats.takeDamage(damage);
+			return "Deathclaw bites you for " + damage + " damage!";
 		}
-		return super.generateResponse(userInput);
+
+		// 10% chance to attack player
+		if (chance < 0.20 && playerStats != null) {
+			int damage = 10;
+			playerStats.takeDamage(damage);
+			return "Deathclaw attacks you with it's claws for " + damage + " damage!";
+		}
+
+		// 30% chance to roar twice
+		if (chance < 0.40) {
+			return getResponses()[0] + " " + getResponses()[0];
+		}
+
+		// Otherwise, return a random normal response
+		int index = (int) (Math.random() * getResponses().length);
+		return getResponses()[index];
 	}
 
 	@Override
@@ -42,4 +66,13 @@ public class Deathclaw extends ChatBot {
 		return "Deathclaw";
 	}
 
+	@Override
+	protected String getTitle() {
+		return "Deathclaw";
+	}
+
+	@Override
+	protected String getIconPath() {
+		return "/PopBoi/Images/DEATHCLAW.png";
+	}
 }
